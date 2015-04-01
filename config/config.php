b3683b4453eac9c97871cae52990f0ccd98109e1
Sonyl Nagale <sonyl@nagale.com>
2015-04-01 16:02:21 -0400

    Blank config

--- config/config.php
+++ config/config.php
@@ -14,13 +14,13 @@ define('SITE_DOMAIN', 'localhost');
 define('SESSION_EXPIRATION', 2592000); // 30 days
 
 define('GIGYA_API_KEY',
-	'2_yM8ACPAsEmp9GNWZaf9pms70VCJ317NzLHdkDRNHsAwIgLFrt1xKmOjmqt1lnTS1');
-define('GIGYA_SECRET', 'Nl41R4RNYQLfRHXBa0qmX6Jlnn8/LDmLj7NPMEB+bwo=');
+	'');
+define('GIGYA_SECRET', '');
 
-define('LIVEFYRE_NETWORK', 'gigya-0.fyre.co');
-define('LIVEFYRE_NETWORK_KEY', 'tZ+tcybHzrMNnfJPnL8TB/3ZHok=');
-define('LIVEFYRE_SITE_ID', '303862');
-define('LIVEFYRE_SITE_KEY', 'SD58lOlEAMpB93j1NzCdqYX1xdw=');
+define('LIVEFYRE_NETWORK', '');
+define('LIVEFYRE_NETWORK_KEY', '');
+define('LIVEFYRE_SITE_ID', '');
+define('LIVEFYRE_SITE_KEY', '');
 
 define('LIVEFYRE_COOKIE_NAME', 'livefyre_token');
 
