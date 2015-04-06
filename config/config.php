71b99c6ef6c3045c7fc8ae066dbc05991ff2a859
Sonyl Nagale <sonyl@nagale.com>
2015-04-06 11:35:31 -0400

    Adding to gitignore and removing stray comments.

--- config/config.php
+++ config/config.php
@@ -1,14 +1,5 @@
 <?php
 
-// define('DOC_ROOT', realpath(dirname(__FILE__) . DIRECTORY_SEPARATOR . '..'));
-// set_include_path(implode(PATH_SEPARATOR, array(
-//     DOC_ROOT . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR . 'livefyre', // Livefyre uses very generic file names and we want it high in our include structure.
-//     DOC_ROOT . DIRECTORY_SEPARATOR . 'lib',
-//     DOC_ROOT . DIRECTORY_SEPARATOR . 'inc',
-//     get_include_path(),
-// )));
-
-
 // PUSH THESE VARS TO ENV
 define('SITE_DOMAIN', 'localhost');
 define('SESSION_EXPIRATION', 2592000); // 30 days
